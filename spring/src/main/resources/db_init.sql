826d314910626753b54f0dbc379d3d12825de896
Kpumuk <bag001@yandex.ru>
2021-06-23 17:07:03 +0300

    update db

--- spring/src/main/resources/db_init.sql
+++ spring/src/main/resources/db_init.sql
@@ -1,18 +1,18 @@
 
 create table films (
-                       id                      bigserial primary key,
+                       id                      int  primary key AUTO_INCREMENT,
                        name                    varchar(80) not null,
                        time                    int not null
 );
 
 create table time_session (
-                              id                      bigserial primary key,
+                              id                      int  AUTO_INCREMENT primary key,
                               film_id                 bigint not null references films (id),
                               begin_film              timestamp
 );
 
 create table tickets (
-                         id                      bigserial primary key,
+                         id                      int  AUTO_INCREMENT primary key,
                          session_id              bigint not null references time_session(id),
                          cost                    int
 );
@@ -48,3 +48,35 @@ values
 (2, 250),  (11, 300), (11, 300), (11, 300), (11, 300),  (12, 300), (12, 300), (12, 300), (12, 300), (12, 300),
 (2, 250);
 
+-- Как оказалось H2 не поддерживает with (((
+select I1.name, I1.begin_film, I1.end_time,
+       I2.name, I2.begin_film, I2.end_time
+from (select A.film_id,
+             A.id as session_id,
+             B.name,
+             A.begin_film,
+             dateadd('MINUTE', B.time, A.begin_film) as end_time
+      from time_session A
+               inner join films B on A.film_id = B.id) I1
+         inner join ( select A.film_id,
+                             A.id as session_id,
+                             B.name,
+                             A.begin_film,
+                             dateadd('MINUTE', B.time, A.begin_film) as end_time
+                      from time_session A
+                               inner join films B on A.film_id = B.id) I2 ON I1.begin_film < I2.end_time AND I1.end_time > I2.begin_film
+    AND I1.session_id != I2.session_id;
+
+-- Колонки «фильм 1», «время начала», «длительность», «время начала второго фильма», «длительность перерыва»;
+select A.name, A.begin_film, A.time,
+       B.name, B.begin_film, datediff('minute', A.end_time, B.begin_film) as time_break
+from (select B.name,
+             B.time,
+             A.begin_film,
+             dateadd('MINUTE', B.time, A.begin_film) as end_time
+      from time_session A
+               inner join films B on A.film_id = B.id) as A
+         inner join (select F.name, B.begin_film
+                     from time_session B
+                              inner join films F on B.film_id = F.id) as B ON B.begin_film > A.end_time
+;
